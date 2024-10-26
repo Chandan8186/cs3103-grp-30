c2fb2e1b4db461f61fd9c7b048a619f2cdc436d1
bryanyee33 <bryanyee33@gmail.com>
2024-10-26 22:19:27 +0800

    Add link expiry date

--- image_link.py
+++ image_link.py
@@ -1,13 +1,16 @@
+from datetime import timedelta, date
 import aiohttp
 import asyncio
 
 # Don't overwhelm the slow server
 REQUEST_LIMIT = 25
+EXPIRY_DATE = (date.today() + timedelta(days=90)).strftime("%m/%d/%Y")
 
-async def make_image_link(session, unique_id) -> str:
+async def _make_image_link(session, unique_id) -> str:
     params = {"url": "https://github.com/Chandan8186/cs3103-grp-30/releases/download/1x1-transparent-image/1x1.png",
               "type": "json",
               "private": "1",
+              "expire": EXPIRY_DATE,
               # Ensures the input is a string, as there is no type checking for unique_id
               "custom": str(unique_id)}
 
@@ -29,15 +32,25 @@ async def make_image_link(session, unique_id) -> str:
 
         return parsed_redirect["data"]["url"]
 
-# Run using: asyncio.run(make_image_links(unique_id_list))
+
+"""
+Asynchronously generates unique links for each unique id.
+Usage: asyncio.run(make_image_links(unique_id_list))
+
+Parameters:
+unique_id_list (list[str]): List of unique ids.
+
+Returns:
+list[str]: List of unique links.
+"""
 async def make_image_links(unique_id_list) -> list[str]:
     conn = aiohttp.TCPConnector(limit=REQUEST_LIMIT)
     async with aiohttp.ClientSession(connector = conn) as session:
-        coroutines = [make_image_link(session, unique_id) for unique_id in unique_id_list]
+        coroutines = [_make_image_link(session, unique_id) for unique_id in unique_id_list]
         return await asyncio.gather(*coroutines)
 
 
-async def get_image_count(session, unique_id) -> int:
+async def _get_image_count(session, unique_id) -> int:
     params = {"type": "json",
               # Ensures the input is a string, as there is no type checking for unique_id
               "id": str(unique_id)}
@@ -60,9 +73,19 @@ async def get_image_count(session, unique_id) -> int:
 
         return int(parsed_redirect["data"]["hits"])
 
-# Run using: asyncio.run(get_image_counts(unique_id_list))
+
+"""
+Asynchronously gets image download count for each unique id.
+Usage: asyncio.run(get_image_counts(unique_id_list))
+
+Parameters:
+unique_id_list (list[str]): List of unique ids.
+
+Returns:
+list[int]: List of image download counts.
+"""
 async def get_image_counts(unique_id_list) -> list[int]:
     conn = aiohttp.TCPConnector(limit=REQUEST_LIMIT)
     async with aiohttp.ClientSession(connector = conn) as session:
-        coroutines = [get_image_count(session, unique_id) for unique_id in unique_id_list]
+        coroutines = [_get_image_count(session, unique_id) for unique_id in unique_id_list]
         return await asyncio.gather(*coroutines)
