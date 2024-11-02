ca7fc12b8a0db0bfd04ddca43355d4a6bad32130
Muhammad Arshad Shaik <97332558+FireRadical22@users.noreply.github.com>
2024-11-02 23:49:55 +0800

    Clean up Code
    Add documentation for SMTP_Connection and Azure_User class
    
    Remove redundant code

--- send_email_test.py
+++ send_email_test.py
@@ -41,34 +41,4 @@ def send_Yahoo_Or_Gmail_Email_To_Eng_Dept_Test():
 
     print(f"Result: {expected_successful_emails_sent == successful_emails_sent}")
 
-def send_Outlook_Email_To_Eng_Dept_Test():
-    sender = "" #Ensure the outlook address your giving matches the client id and client secret given
-    user_id = "" #Client-ID obtained from Microsoft Azure
-    password = "" #Client-secret generated from Microsoft Azure
-    department = "Eng"
-
-    outlook_test = OutlookEmailSender(user_id, password)
-    parser_test = Parser(sender, "uploads/test.csv", "uploads/body.txt")
-
-    # Using Public Client Flow
-    if (not outlook_test.authenticate()):
-        print("Could not Authenticate!")
-        sys.exit(1)
-    
-    emails_to_send = parser_test.prepare_all_emails(department)
-
-    successful_emails_sent = 0
-    expected_successful_emails_sent = len(emails_to_send)
-
-    for email in emails_to_send:
-        recipient = email['email']
-        subject = email['subject']
-        body = email['body']
-
-        if (outlook_test.send_email(recipient, subject, body)):
-            successful_emails_sent += 1
-    
-    print(successful_emails_sent == expected_successful_emails_sent)
-
 #send_Yahoo_Or_Gmail_Email_To_Eng_Dept_Test()
-#send_Outlook_Email_To_Eng_Dept_Test()
